431acd4e92b5dcab47d69bc739e088450fc0628d
Tuan Nguyen <54676613+minhtuan1407@users.noreply.github.com>
2022-05-13 11:38:12 +0700

    Create merge-git.sh

---
+++ merge-git.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+all_branches=$(git ls-remote --heads origin | sed 's?.*refs/heads/??')
+
+for branches in $all_branches; do
+    git cherry-pick --abort
+    git pull --all --no-edit
+done
+
+commit_id_with_hosts=$(git log -1 --all --format=%H hosts_all_server)
+
+for branches in $all_branches; do
+    if [[ $branches =~ ^main-* ]] ; then
+        # echo $branches
+        git checkout $branches
+        git pull --all --no-edit
+        git cherry-pick $commit_id_with_hosts
+        git checkout --theirs file_name
+        git add file_name
+        git commit -m "change: script update file_name"
+        git push origin $branches
+    fi
+done
